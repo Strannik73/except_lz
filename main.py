b8f774411f528bba3ef5cffaca24a9d9987676db
Strannik37 <Santon@tutamail.com>
2025-05-16 19:51:22 +0300

    obr.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from obr import obrabotka
+
+def main():
+    obr_a = obrabotka('var3.csv')
+    obr_a = obrabotka('var3i.csv')
+    obr_a.dann()
+
+if __name__ == '__main__':
+    main()
--- obr.py
+++ obr.py
@@ -1,18 +1,61 @@
 import pandas as pd
+#from __ import __
 
 
-class sumop:
-
-    def __init__(self):
+class obrabotka:
+    def __init__(self): 
         self.df = pd.read_csv('var3.csv')
-
-    def __neg__(self):
-        return self.df.drop_duplicates()
-
-
-    def dann(self):
-        df = -self.df
+        self.dfi = pd.read_csv('var3i.csv')
         
-        columns = ['Участники гражданского оборота','Тип операции','Сумма операции','Вид расчета','Место оплаты','Терминал оплаты','Дата оплаты','Время оплаты','Результат операции','Cash-back,Сумма cash-back']
-        if not all(col in self.data.columns for col in columns):
-            raise ValueError('CSV файл должен содержать колонки')
+        try:
+            self.df = self.dfi
+        except FileNotFoundError:
+            print('файла нет')
+            raise
+        except pd.errors.EmptyDataError:
+            print('файл пустой')
+            raise
+        
+        try:    
+            if self.df != self.dfi:
+                raise TypeError 
+        except TypeError:
+            print('ошибка в колонках')
+            
+        try:
+            self.v3 = str(self.df.dtypes)
+            self.v3i = str(self.dfi.dtypes)
+            if self.v3 != self.v3i: 
+                print('другой тип данных')
+                raise TypeError 
+            else:
+                print('есть совпадение')
+        except TypeError:
+            print('несовпадение файлов ожидплся изменный файл')
+                
+        
+    def __del__(self):
+        print('удаление')
+
+
+def main():
+    obr_a = obrabotka('var3.csv')
+    obr_a = obrabotka('var3i.csv')
+    obr_a.dann()
+
+if __name__ == '__main__':
+    main()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
